559790b1bfe26c5f6a2685a5112bbd62f3d8abcf
Samsung120 <Samsung@sch120.ru>
2025-02-24 22:12:41 +0600

    direction of fragments

--- core/src/main/java/ru/samsung/raidespacex/Fragment.java
+++ core/src/main/java/ru/samsung/raidespacex/Fragment.java
@@ -5,7 +5,10 @@ import static ru.samsung.raidespacex.Main.*;
 import com.badlogic.gdx.math.MathUtils;
 
 public class Fragment extends SpaceObject{
-    int numImg;
+    public int numImg;
+    public float rotation;
+    private float vRotation;
+
     public Fragment(float x, float y, int type, int numImgFragmets) {
         super(x, y);
         this.type = type;
@@ -16,6 +19,13 @@ public class Fragment extends SpaceObject{
         float v = MathUtils.random(2f, 10f);
         vx = v*MathUtils.sin(a);
         vy = v*MathUtils.cos(a);
+        vRotation = MathUtils.random(-5f, 5f);
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        rotation += vRotation;
     }
 
     public boolean outOfScreen(){
